7406c8312f6daa4d108f0aa28e0dd8d383870de8
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-16 00:28:22 +0100

    Textos de cada pantalla

--- Assets/Teletrans.cs
+++ Assets/Teletrans.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Teletrans : MonoBehaviour
 {
     public GameObject objetivo; 
+    public bool necesitaTexto;
+    public string nombreSitio; 
+    public GameObject texto;
+    public Text textolugar; 
 
  
     void Awake()
@@ -27,9 +32,22 @@ public class Teletrans : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Player")
+        if (other.tag.Equals("Player"))
         {
             other.transform.position = objetivo.transform.GetChild(0).transform.position; 
+            if (necesitaTexto)
+            {
+                StartCoroutine(gestionTexto()); 
+            }
         }
+    }
+
+
+    private IEnumerator gestionTexto()
+    {
+        texto.SetActive(true); 
+        textolugar.text = nombreSitio;
+        yield return new WaitForSeconds(4f);
+        texto.SetActive(false); 
     }
 }
