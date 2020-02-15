33982decc5da3072ced8fd1c9d12d9fa830de723
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-15 21:15:15 +0100

    Comenzando Warp Points

---
+++ Assets/Teletrans.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teletrans : MonoBehaviour
+{
+    public GameObject objetivo; 
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
