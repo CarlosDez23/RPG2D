4f67df30d24156cc6cde999ec5d93d18c0f3c21d
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-15 19:21:39 +0100

    Archivos del proyecto

---
+++ Assets/Scripts/MovimientoCamara.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour
+{
+   
+    public Transform objetivo; 
+    public float smoothing; 
+    public Vector2 maxPosicion;
+    public Vector2 minPosicion; 
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+    //Se mueve despues del update del movimiento del personaje en cada frame
+    void LateUpdate()
+    {
+        if (transform.position != objetivo.position)
+        {
+            Vector3 posicionObjetivo = new Vector3(objetivo.position.x, objetivo.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position,posicionObjetivo,smoothing);   
+
+        }
+    }
+}
---
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{   
+
+    public float velocidad = 4f; 
+
+    Animator animator;
+    Rigidbody2D rigidbody; 
+    Vector2 movimiento;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        rigidbody = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        movimiento = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")    
+        ); 
+
+        //Solo actualizamos la animación cuando nos estemos moviendo
+        if (movimiento != Vector2.zero)
+        {
+            animator.SetFloat("movX", movimiento.x);
+            animator.SetFloat("movY", movimiento.y);  
+            animator.SetBool("andando", true); 
+        } else
+        {
+            animator.SetBool("andando", false); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movimiento * velocidad * Time.deltaTime);
+    }
+}
