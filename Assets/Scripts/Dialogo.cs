434ab8de09931f323340d609ea1e70d6c05a1f84
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-16 01:29:14 +0100

    Cartes con dialogos funcionando

---
+++ Assets/Scripts/Dialogo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Dialogo : MonoBehaviour
+{
+    public GameObject cajaDialogo;
+    public Text textoDialogo; 
+    public string texto;
+    public bool activo; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space) && activo)
+        {
+            if (cajaDialogo.activeInHierarchy)
+            {
+                cajaDialogo.SetActive(false);
+            }else
+            {
+                cajaDialogo.SetActive(true); 
+                textoDialogo.text = texto; 
+            }
+        }
+    }
+
+    //Cuando el jugador dispare el trigger del cartel 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            //Mostramos el diálogo
+            activo = true; 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.tag.Equals("Player"))
+        {
+            activo = false; 
+            cajaDialogo.SetActive(false);
+        }
+    }
+}
