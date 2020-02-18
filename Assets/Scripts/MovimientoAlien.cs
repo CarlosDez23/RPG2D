e16b69f8b2bbaf53e7a9198d83d8ffe6491bab6f
Adrian <adrianscc11@gmail.com>
2020-02-18 19:49:16 +0100

    Retocando Enemigo

--- Assets/Scripts/MovimientoAlien.cs
+++ Assets/Scripts/MovimientoAlien.cs
@@ -13,10 +13,6 @@ public class MovimientoAlien : MonoBehaviour
     Rigidbody2D rigidbody2;
     GameObject personaje;
 
-
-    Vector2 mov;
-
-
     // Start is called before the first frame update
     void Start()
     {
