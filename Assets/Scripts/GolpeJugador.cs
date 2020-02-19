955042d7020727572846f0c27bf97ac0eaf59639
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-19 18:11:58 +0100

    Jarrones rompibles

---
+++ Assets/Scripts/GolpeJugador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolpeJugador : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag.Equals("rompible"))
+        {
+            other.GetComponent<Jarron>().destruirJarron(); 
+        }
+    }
+}
---
+++ Assets/Scripts/Jarron.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jarron : MonoBehaviour
+{   
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void destruirJarron()
+    {
+        animator.SetBool("golpear",true); 
+        StartCoroutine(romper());
+    }
+
+    IEnumerator romper()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+        Destroy(this); 
+    }
+}
