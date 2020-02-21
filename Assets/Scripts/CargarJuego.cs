f0df9fcc0231dc7c37a4b9a74b709ea2c249c919
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-21 21:52:22 +0100

    Guardando y cargando partida funcionando

---
+++ Assets/Scripts/CargarJuego.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarJuego
+{
+    public static GameObject player; 
+
+  //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void cargar()
+    {
+        Debug.Log("Ha pasado a cargar");
+        DatosJugador datos = SistemaGuardado.cargarPartida();
+        if (datos != null)
+        {
+            Debug.Log(datos.escena.ToString()+" "+datos.vidas+" "+datos.llaves+" "+datos.posicion[0]);
+            string escena = datos.escena;
+            SceneManager.LoadScene(escena);
+            player = GameObject.FindGameObjectWithTag("Player");
+            Debug.Log(player.tag);
+            player.GetComponent<MovimientoPersonaje>().cargarPersonaje(datos);
+        }
+    }
+}
--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -3,45 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MovimientoPersonaje : MonoBehaviour
-{   
+{
 
-    public float velocidad = 4f; 
+    public float velocidad = 4f;
     Animator animator;
-    Rigidbody2D rigidbody; 
+    Rigidbody2D rigidbody;
     Vector2 movimiento;
-    CircleCollider2D colliderAtaque; 
+    CircleCollider2D colliderAtaque;
 
-    public int vidas; 
-    public int llaves; 
-    
+    public int vidas;
+    public int llaves;
+
+
     //Prueba Inicio Mapa Nivel 2
     void Start()
     {
-        animator = GetComponent<Animator>(); 
-        rigidbody = GetComponent<Rigidbody2D>(); 
-        colliderAtaque = transform.GetChild(0).GetComponent<CircleCollider2D>(); 
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        colliderAtaque = transform.GetChild(0).GetComponent<CircleCollider2D>();
         //Lo desactivamos desde el principio
-        colliderAtaque.enabled = false; 
-        this.vidas  = 5; 
-        this.llaves = 0; 
+        colliderAtaque.enabled = false;
+        //Gestionamos la carga de la partida
+        DatosJugador datos = SistemaGuardado.cargarPartida();
+        if (datos != null)
+        {
+            this.vidas = datos.vidas;
+            this.llaves = datos.llaves;
+            this.transform.position = new Vector2(datos.posicion[0], datos.posicion[1]);
+        }
+        else
+        {
+            this.vidas = 4;
+            this.llaves = 0;
+        }
     }
 
     void Update()
     {
         movimiento = new Vector2(
             Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical")    
-        ); 
+            Input.GetAxisRaw("Vertical")
+        );
 
         //Solo actualizamos la animación cuando nos estemos moviendo
         if (movimiento != Vector2.zero)
         {
             animator.SetFloat("movX", movimiento.x);
-            animator.SetFloat("movY", movimiento.y);  
-            animator.SetBool("andando", true); 
-        } else
+            animator.SetFloat("movY", movimiento.y);
+            animator.SetBool("andando", true);
+        }
+        else
         {
-            animator.SetBool("andando", false); 
+            animator.SetBool("andando", false);
         }
 
         //Miramos el estado actual en el animador
@@ -50,29 +63,30 @@ public class MovimientoPersonaje : MonoBehaviour
         //No dejamos atacar hasta que no termine la animación 
         if (Input.GetKeyDown(KeyCode.Space) && !atacando)
         {
-            animator.SetTrigger("atacando"); 
+            animator.SetTrigger("atacando");
         }
 
         //Actualizamos la posición del collider de ataque en función de donde está mirando el personaje 
         if (movimiento != Vector2.zero)
         {
-            colliderAtaque.offset = new Vector2(movimiento.x/2, movimiento.y/2);
+            colliderAtaque.offset = new Vector2(movimiento.x / 2, movimiento.y / 2);
         }
 
         if (atacando)
         {
             //Normalizedtime guarda el tiempo que dura la animación
-            float tiempoAnimacion = info.normalizedTime; 
+            float tiempoAnimacion = info.normalizedTime;
             //Si lo dividimos en 3 tercios, y activamos el collider del ataque en el segundo tercio
             //conseguimos que el collider solo este activo en mitad de la animación
             if (tiempoAnimacion > 0.33 && tiempoAnimacion < 0.66)
             {
-                colliderAtaque.enabled = true; 
-                StartCoroutine(guardar()); 
-                
-            }else
+                colliderAtaque.enabled = true;
+                StartCoroutine(guardar());
+
+            }
+            else
             {
-                colliderAtaque.enabled = false; 
+                colliderAtaque.enabled = false;
             }
         }
     }
@@ -81,18 +95,17 @@ public class MovimientoPersonaje : MonoBehaviour
     {
         rigidbody.MovePosition(rigidbody.position + movimiento * velocidad * Time.deltaTime);
     }
-    
+
     IEnumerator guardar()
     {
         yield return new WaitForSeconds(.3f);
-        SistemaGuardado.guardarPartida(this, SceneManager.GetActiveScene().name); 
+        SistemaGuardado.guardarPartida(this, SceneManager.GetActiveScene().name);
     }
 
     public void cargarPersonaje(DatosJugador datos)
     {
-        movimiento.x = datos.posicion[0];
-        movimiento.y = datos.posicion[1]; 
+        Debug.Log("Datos recibidos a cargar " + datos.posicion[0] + " " + datos.posicion[1]);
         this.vidas = datos.vidas;
-        this.llaves = datos.llaves; 
+        this.llaves = datos.llaves;
     }
 }
