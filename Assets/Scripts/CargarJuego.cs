2960e0c9bd563c72b5bda2c2e33d24376e686d1e
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-24 19:50:56 +0100

    Actualizado el script de cargar la escena

--- Assets/Scripts/CargarJuego.cs
+++ Assets/Scripts/CargarJuego.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CargarJuego
+public static class CargarJuego
 {
-    public static GameObject player; 
 
-  //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-    static void cargar()
+    public static void cargar()
     {
-        Debug.Log("Ha pasado a cargar");
         DatosJugador datos = SistemaGuardado.cargarPartida();
         if (datos != null)
         {
