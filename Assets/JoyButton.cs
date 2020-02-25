895e8324f0f81e1c4514f5478449883f4c8e7e81
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-25 21:57:58 +0100

    Empezando con los joystick

---
+++ Assets/JoyButton.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool pulsado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pulsado = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pulsado = false;
+    }
+}
