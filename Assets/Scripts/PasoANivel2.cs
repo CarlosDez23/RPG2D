d05b785fd5a398a95c93773f35c689b790e38642
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-25 18:38:23 +0100

    Funcionando transición al nivel2

---
+++ Assets/Scripts/PasoANivel2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasoANivel2 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player") && other.GetComponent<MovimientoPersonaje>().llaves == 6)
+        {
+            SceneManager.LoadScene("Nivel2");
+        }
+    }
+}
